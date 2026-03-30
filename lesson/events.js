4d4b51711bc7b68e8b5d775095dc5fc3fcf279c4
Кондусов Александр Александрович <kondusov_aa@ACADEMY.TOP>
2026-03-30 16:08:52 +0300

    dz

--- lesson/events.js
+++ lesson/events.js
@@ -1,4 +1,16 @@
 let mouseElem = document.getElementById('mouseEnter');
 mouseElem.addEventListener('mouseenter', ()=>{
     console.log('курсор пришел на элемент')
+    document.getElementById('resultDataForm').textContent = 'курсор пришел на элемент';
+})
+mouseElem.addEventListener('mouseleave', ()=>{
+    console.log('курсор покинул элемент')
+    document.getElementById('resultDataForm').textContent = 'курсор покинул элемент';
+})
+
+let mouseEntwerBlock = document.getElementById('mouseEnter');
+mouseEntwerBlock.style.color = '#fff';
+document.addEventListener('mousemove', (event)=>{
+    console.log('по Х:'+ event.clientX+' по Y:'+ event.clientY)
+    mouseEntwerBlock.textContent = `по Х: ${event.clientX} по Y: ${event.clientY}`;
 })
